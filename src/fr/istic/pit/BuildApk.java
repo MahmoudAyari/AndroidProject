4dd351b0ec5ec7ee9f016f2139d2c951242a27d3
MahmoudAyari <ayari.mahmoud@outlook.fr>
2016-03-01 13:14:54 +0100

    ok

--- src/fr/istic/pit/BuildApk.java
+++ src/fr/istic/pit/BuildApk.java
@@ -5,62 +5,70 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
 import javafx.application.Platform;
-import sun.tools.jar.CommandLine; 
- 
-public class BuildApk
-{
-  public static void main(String[] args) 
-  throws IOException, InterruptedException
-  {
-	  
-	  String projectPath ="/home/ayari/AndroidStudioProjects/Tp3";
-	  buildApk(projectPath);
-  }
-  
-  
-  public static void buildApk(String projectPath){
 
-	  try{  
+public class BuildApk {
 
-		  ExecutorService executor = Executors.newSingleThreadExecutor();
-		    executor.submit(() -> {
+	public OsValidator OS;
+	public ProcessBuilder builder;
 
-		       //------------------------------------Form windows --------------------------------//
-		       // ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/C", projectPath + "/gradlew assembleRelease -p" + projectPath, "--info");
-		    	
-		    	//------------------------------------Form unix-----------------------------------//
-		    	ProcessBuilder builder = new ProcessBuilder("/bin/bash" ,"-c",  projectPath + "/gradlew assembleDebug -p" + projectPath, "--info");
-		        builder.redirectErrorStream(true);
-		        try {
-		            Process p = builder.start();
-		            BufferedReader stdout = new BufferedReader(
-		                    new InputStreamReader(p.getInputStream()));
+	public BuildApk() {
+		OS = new OsValidator();
+	}
 
-		            System.out.println("outputString:: " + stdout.readLine());
-		            String s = "";
-		            while (  (s=stdout.readLine())  != null) {
-		                System.out.println("outputString:: " + s);
-		                
-		                Platform.runLater(() -> {
-		                    //if you change the UI, do it here 
-		                });
-		                
-		                if (stdout.readLine().contains("BUILD SUCCESSFUL")) {
-		                    break;
-		                }
-		            }
-		            p.getInputStream().close();
-		            p.getOutputStream().close();
-		            p.getErrorStream().close();
-		            p.destroy();
-		        } catch (IOException e) {
-		            e.printStackTrace();
-		        }
-		    });
-		    
-	  }catch( Exception e){
-		   }
-  }
+//	 public static void main(String[] args)
+//	 throws IOException, InterruptedException
+//	 {
+//		 OS = new OsValidator();
+//	 String projectPath ="/home/ayari/AndroidStudioProjects/Tp3";
+//	 buildApk(projectPath);
+//	 }
+	
+
+	public void buildApk(String projectPath) {
+
+		try {
+
+			ExecutorService executor = Executors.newSingleThreadExecutor();
+			executor.submit(() -> {
+
+				if (OS.isWindows()) {
+					builder = new ProcessBuilder("cmd.exe", "/C",
+							projectPath + "/gradlew assembleRelease -p" + projectPath, "--info");
+
+				} else if (OS.isUnix()) {
+					builder = new ProcessBuilder("/bin/bash", "-c",
+							projectPath + "/gradlew assembleDebug -p" + projectPath, "--info");
+				}
+
+				builder.redirectErrorStream(true);
+				try {
+					Process p = builder.start();
+					BufferedReader stdout = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+					System.out.println("outputString:: " + stdout.readLine());
+					String s = "";
+					while ((s = stdout.readLine()) != null) {
+						System.out.println("outputString:: " + s);
+
+						Platform.runLater(() -> {
+							// if you change the UI, do it here
+						});
+
+						if (stdout.readLine().contains("BUILD SUCCESSFUL")) {
+							break;
+						}
+					}
+					p.getInputStream().close();
+					p.getOutputStream().close();
+					p.getErrorStream().close();
+					p.destroy();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+
+		} catch (Exception e) {
+		}
+	}
 }
--- src/fr/istic/pit/GenerateAndroidProject.java
+++ src/fr/istic/pit/GenerateAndroidProject.java
@@ -1,38 +1,54 @@
 package fr.istic.pit;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import javafx.application.Platform; 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream; 
+import java.io.PrintWriter;
 
-import javafx.application.Platform;
-import sun.tools.jar.CommandLine; 
- 
 public class GenerateAndroidProject
 {
-  public static void main(String[] args) 
-  throws IOException, InterruptedException
-  {
-	  
-	  String toolsPath ="/home/ayari/android-sdk-linux/tools";
-	  generateAndroidProject(toolsPath);
-  }
+
+  public static OsValidator OS ;
+  public static ProcessBuilder builder;
   
+  public GenerateAndroidProject() { 
+	 OS = new OsValidator() ;
+	}
+
   
-  public static void generateAndroidProject(String toolsPath){
+  public static void main(String[] args)
+			 throws IOException, InterruptedException
+			 {
+				 OS = new OsValidator(); 
+			 generateAndroidProject("/home/ayari/android-sdk-linux/tools","1.2.2",1,"rtest1","/home/ayari/Bureau/rtest1","MainActivity","com.mydomain.rtest");
+			  
+			 }
+  
+  public static void generateAndroidProject(String toolsPath,String gradleVersion,int targetNumber, String nameProject,String pathOfProject,String nameActivity,String namePackage){
 
 	  try{  
 
 		  ExecutorService executor = Executors.newSingleThreadExecutor();
 		    executor.submit(() -> {
-
-		       //------------------------------------Form windows --------------------------------//
-		       // ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/C", projectPath + "/gradlew assembleRelease -p" + projectPath, "--info");
 		    	
-		    	//------------------------------------Form unix-----------------------------------//
-		    	ProcessBuilder builder = new ProcessBuilder("/bin/bash" ,"-c",  toolsPath + "/android create project -g -v 1.2.2 --target 1 --name rtest --path rtest --activity MainActivity --package com.mydomain.rtest");
-		        builder.redirectErrorStream(true);
+		    	
+                if(OS.isWindows()){
+		           builder = new ProcessBuilder("cmd.exe", "/C",  toolsPath + "/android create project -g -v "+gradleVersion+" --target "+targetNumber+" --name "+nameProject+" --path "+pathOfProject+" --activity "+nameActivity+" --package "+namePackage);
+                } else if (OS.isUnix()) {
+				
+		    	   builder = new ProcessBuilder("/bin/bash" ,"-c",  toolsPath + "/android create project -g -v "+gradleVersion+" --target "+targetNumber+" --name "+nameProject+" --path "+pathOfProject+" --activity "+nameActivity+" --package "+namePackage);
+		         }
+                
+                
+		    	builder.redirectErrorStream(true);
 		        try {
 		            Process p = builder.start();
 		            BufferedReader stdout = new BufferedReader(
@@ -49,10 +65,19 @@ public class GenerateAndroidProject
 		                
 		                
 		            }
+		            
+		            replaceFile("/home/ayari/Bureau/rtest1/gradle/wrapper/gradle-wrapper.properties",
+		            		"/home/ayari/Bureau/rtest/gradle/wrapper/gradle-wrapper.properties");
+		            
 		            p.getInputStream().close();
 		            p.getOutputStream().close();
 		            p.getErrorStream().close();
 		            p.destroy();
+		            
+		            
+		        
+		            
+		            
 		        } catch (IOException e) {
 		            e.printStackTrace();
 		        }
@@ -61,4 +86,32 @@ public class GenerateAndroidProject
 	  }catch( Exception e){
 		   }
   }
-}
+  
+   
+  public static void replaceFile(String source,String dest) throws IOException{
+	        
+	  File fin = new File(source);
+	    FileInputStream fis = new FileInputStream(fin);
+	    BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+	    FileWriter fstream = new FileWriter(dest, true);
+	    BufferedWriter out = new BufferedWriter(fstream);
+
+	    String aLine = null; 
+	    
+	    PrintWriter pw = new PrintWriter(new File(dest));
+	    pw.write("");
+	    pw.flush();
+	    
+	    
+	    while ((aLine = in.readLine()) != null) { 
+	        out.write(aLine);
+	        out.newLine();
+	    }
+ 
+	    in.close();
+        out.close();
+	     }
+	}
+  
+ 
